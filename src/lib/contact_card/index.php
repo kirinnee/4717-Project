06035aa5be11da729ace8e185baceac3eb4852fe
kirinnee <kirinnee97@gmail.com>
2022-11-02 18:17:55 +0000

    feat: cleaned up contact page

--- src/lib/contact_card/index.php
+++ src/lib/contact_card/index.php
@@ -1,52 +1,52 @@
-<html>
-    <body>
-    <div class="contact">
-        <div class="box-main">
-
-            <h3>About us</h3>
-
-            <div class="firstHalf">
-                <div id="founder">
-                    <img src=".\lib\images\stock-founder.jpg" alt="Picture_1">
-                    <h3>The Founders</h3>
-                    <p class="center" style="text-decoration:none;color:white;">
-                    The projectionist is a passion project by a group of friends that love with the cinema.                    </p>
-                </div>
-                <br>
-                <div id="team">
-                    <img src=".\lib\images\stock-team.jpg" alt="Picture_2">
-                    <h3>The Team</h3>
-                    <p class="center" style="text-decoration:none;color:white;">
-                    The projectionist team is made up of like minded film enthusiasts that want to spread love and joy through the silver screen.
-                    </p>
-                </div>
-            </div>
+<h1>Projectionist</h1>
+<div class="holder">
+    <div class="clip"></div>
+    <div class="content">
+        <div class="words">
+            <div class="big">PASSIONATE</div>
+            <div class="light">FOUNDERS</div>
+            <div class="desc">The projectionist is a passion project by a group of friends that love with the cinema.</div>
+        </div>
+        <div class="pictures">
+            <img src=".\lib\images\stock-founder.jpg" alt="Picture_1">
         </div>
     </div>
-    <div class="service" >
-        <h1 id="head">
-            Looking for more?</h1>
-        <div id="service">
-            <div class="box">
-                <img src=".\lib\images\corporate-event.png" alt= "color_image">
-                <br>
-                <p class="center">Book our cinema for events!</p>
-            </div>
-            <div class="box">
-                <img src=".\lib\images\chair.png" alt= "color_image">
-                <br>
-                <p class="center">
-                    Help contribute to the cinema by adopting a chair!
-                </p>
-             </div>
-            <div class="box">
-                <img src=".\lib\images\announcement.png" alt= "color_image">
-                <br>
-                <p class="center">
-                    Looking to be part of the team?
-                </p>
-            </div>
+    <div class="content">
+        <div class="pictures">
+            <img src=".\lib\images\stock-team.jpg" alt="Picture_2">
+
+        </div>
+        <div class="words">
+            <div class="big">AWESOME</div>
+            <div class="light">TEAM</div>
+            <div class="desc">The projectionist team is made up of like minded film enthusiasts that want to spread love and joy through the silver screen.</div>
         </div>
     </div>
-    </body>
-</html>
+</div>
+
+
+<div class="service" >
+    <h1 id="head">
+        Looking for more?</h1>
+    <div id="service">
+        <div class="box">
+            <img src=".\lib\images\corporate-event.png" alt= "color_image">
+            <br>
+            <p class="center">Book our cinema for events!</p>
+        </div>
+        <div class="box">
+            <img src=".\lib\images\chair.png" alt= "color_image">
+            <br>
+            <p class="center">
+                Help contribute to the cinema by adopting a chair!
+            </p>
+         </div>
+        <div class="box">
+            <img src=".\lib\images\announcement.png" alt= "color_image">
+            <br>
+            <p class="center">
+                Looking to be part of the team?
+            </p>
+        </div>
+    </div>
+</div>
--- src/lib/head.php
+++ src/lib/head.php
@@ -6,8 +6,7 @@ return function($title, $css, $js, $defered) {
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Libre+Barcode+39&Poppins:wght@100;200;300;400&family=Raleway:wght@100;200;300;400&display=swap" rel="stylesheet">
-
+<link href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;700&family=Libre+Barcode+39&family=Poppins:wght@100;200;300;400&family=Raleway:wght@100;200;300;400&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="index.css">
 EOL;
     foreach ($css as $v) {
